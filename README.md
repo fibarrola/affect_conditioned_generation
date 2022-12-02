5518760134681ebd5e043b929da03b19ac5b2f05
Francisco Ibarrola <fibarrola@sinc.unl.edu.ar>
2022-12-02 11:40:59 +1100

    fixed options and makefile

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # affect_conditioned_generation
+
+
+### VQGAN model download
+curl -L -o vqgan_imagenet_f16_16384.yaml -C - 'https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1' #ImageNet 16384
+curl -L -o vqgan_imagenet_f16_16384.ckpt -C - 'https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1' #ImageNet 16384
+
+## Usage
+
+### Evaluate affect
+
+```
+python3 get_affect.py
+--prompt <str>          The text prompt if we want a prompt's affect
+--img_path <str>        Path to the image if we want an image's affect
+--format <str>          Affect display format ('score', 'uniform' or 'latex')
+```
+
+### Train MLP
+
+```
+python3 train_mlp.py
+--num_epochs <int>      number of training epochs
+--scaling <str>         scaling for input and output data. Can be 'uniform', 'whiten', 'normalize' or 'none'
+--lr <float>            learning rate
+--layer_dims <str>      layer dimensions. Separate with |
+--use_dropout <bool>    Use dropout for training?
+--use_sigmoid <bool>    Use sigmoid at the end of last layer?
+```
+
+### Generate image with specific affect scores
+```
+python3 generate.py
+--prompt <str>          Prompt of which to get affect score
+--E <float>             Evaluation score (bad<good) in [0,1], default=None
+--P <float>             Potency score (weak<strong) in [0,1], default=None,
+--A <float>             Activity (calm<exciting) in [0,1], default=None,
+--max_iter <int>        Number of algorithm iterations
+```
--- train_mlp.py
+++ train_mlp.py
@@ -2,19 +2,17 @@ from src.mlp import MLP
 from src.data_handler_mixed_sd import DataHandler
 import torch
 import pickle
+import argparse
 from torch.nn.functional import sigmoid
 
-config = {
-    "num_epochs": 2500,
-    "scaling": "uniform",  # 'uniform', 'whiten', 'normalize', 'none'
-    "lr": 0.00007,
-    "num_trials": 1,
-}
-print("---- Config ----")
-for c in config:
-    print(f"{c}: {config[c]}")
-print("")
-config = type('obj', (object,), config)
+parser = argparse.ArgumentParser(description='Training Parameters')
+parser.add_argument("--num_epochs", type=int, default=2500)
+parser.add_argument("--scaling", type=str, default="uniform", help="scaling for input and output data. Can be 'uniform', 'whiten', 'normalize' or 'none'")
+parser.add_argument("--lr", type=float, help="learning rate", default=0.00007)
+parser.add_argument("--layer_dims", type=str, help="layer dimensions. Separate with |", default="64|32")
+parser.add_argument("--use_dropout", type=bool, help="Use dropout for training?", default=True)
+parser.add_argument("--use_sigmoid", type=bool, help="Use sigmoid at the end of last layer?", default=False)
+config = parser.parse_args()
 
 data_handler = DataHandler("data/Ratings_Warriner_et_al.csv")
 data_handler.preprocess(scaling=config.scaling)
@@ -23,8 +21,8 @@ data_handler.build_datasets()
 with open('data/data_handler_mixed.pkl', 'wb') as f:
     pickle.dump(data_handler, f)
 
-
-mlp = MLP([64, 32]).to('cuda:0')
+layer_dims = [int(x) for x in config.layer_dims.split('|')]
+mlp = MLP(layer_dims, do=config.use_dropout, sig=config.use_sigmoid).to('cuda:0')
 print("---- MLP parameters ----\n", mlp.parameters, "\n")
 
 criterion = torch.nn.MSELoss(reduction='mean')
@@ -41,107 +39,86 @@ def threshold_count(x, y, t):
     return r
 
 
-for trial in range(config.num_trials):
-    valid_loss_min = 1e8
-    for epoch in range(config.num_epochs):
-        # monitor losses
-        train_loss = 0
-        valid_loss = 0
-        l1_loss_txt = 0
-        l1_loss_img = 0
-        r_txt = 0
-        r_img = 0
+valid_loss_min = 1e8
+for epoch in range(config.num_epochs):
+    # monitor losses
+    train_loss = 0
+    valid_loss = 0
+    l1_loss_txt = 0
+    l1_loss_img = 0
+    r_txt = 0
+    r_img = 0
 
-        ###################
-        # train the model #
-        ###################
-        mlp.train()  # prep model for training
-        for data, label, _ in data_handler.txt_train_loader:
-            optimizer.zero_grad()
-            output = mlp(data, train=True)
+    ###################
+    # train the model #
+    ###################
+    mlp.train()  # prep model for training
+    for data, label, _ in data_handler.txt_train_loader:
+        optimizer.zero_grad()
+        output = mlp(data, train=True)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * data.size(0)
+
+    for data, label, _ in data_handler.img_train_loader:
+        optimizer.zero_grad()
+        output = mlp(data, train=True)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * data.size(0)
+
+    ######################
+    # validate the model #
+    ######################
+    with torch.no_grad():
+        mlp.eval()  # prep model for evaluation
+        for data, label, sds in data_handler.txt_test_loader:
+            output = mlp(data)
+            label = data_handler.scaler_V.unscale(label)
+            output = data_handler.scaler_V.unscale(output)
             loss = criterion(output, label)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item() * data.size(0)
+            valid_loss += loss.item() * data.size(0)
+            l1_loss_txt += torch.sum(torch.abs(output - label) / 8.6).item()
+            r_txt += threshold_count(output, label, sds) * data.size(0)
 
-        for data, label, _ in data_handler.img_train_loader:
-            optimizer.zero_grad()
-            output = mlp(data, train=True)
+        for data, label, sds in data_handler.img_test_loader:
+            output = mlp(data)
+            label = data_handler.scaler_V.unscale(label)
+            output = data_handler.scaler_V.unscale(output)
             loss = criterion(output, label)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item() * data.size(0)
+            valid_loss += loss.item() * data.size(0)
+            l1_loss_img += torch.sum(torch.abs(output - label) / 8.6).item()
+            r_img += threshold_count(output, label, sds) * data.size(0)
 
-        ######################
-        # validate the model #
-        ######################
-        with torch.no_grad():
-            mlp.eval()  # prep model for evaluation
-            for data, label, sds in data_handler.txt_test_loader:
-                output = mlp(data)
-                label = data_handler.scaler_V.unscale(label)
-                output = data_handler.scaler_V.unscale(output)
-                loss = criterion(output, label)
-                valid_loss += loss.item() * data.size(0)
-                l1_loss_txt += torch.sum(torch.abs(output - label) / 8.6).item()
-                r_txt += threshold_count(output, label, sds) * data.size(0)
+        train_loss = train_loss / (
+            len(data_handler.txt_train_loader.sampler)
+            + len(data_handler.img_train_loader.sampler)
+        )
+        valid_loss = valid_loss / (
+            len(data_handler.txt_test_loader.sampler)
+            + len(data_handler.img_test_loader.sampler)
+        )
+        l1_loss_txt = l1_loss_txt / (3 * len(data_handler.txt_test_loader.sampler))
+        l1_loss_img = l1_loss_img / (3 * len(data_handler.img_test_loader.sampler))
+        r_txt = r_txt / len(data_handler.txt_test_loader.sampler)
+        r_img = r_img / len(data_handler.img_test_loader.sampler)
 
-            for data, label, sds in data_handler.img_test_loader:
-                output = mlp(data)
-                label = data_handler.scaler_V.unscale(label)
-                output = data_handler.scaler_V.unscale(output)
-                loss = criterion(output, label)
-                valid_loss += loss.item() * data.size(0)
-                l1_loss_img += torch.sum(torch.abs(output - label) / 8.6).item()
-                r_img += threshold_count(output, label, sds) * data.size(0)
-
-            train_loss = train_loss / (
-                len(data_handler.txt_train_loader.sampler)
-                + len(data_handler.img_train_loader.sampler)
-            )
-            valid_loss = valid_loss / (
-                len(data_handler.txt_test_loader.sampler)
-                + len(data_handler.img_test_loader.sampler)
-            )
-            l1_loss_txt = l1_loss_txt / (3 * len(data_handler.txt_test_loader.sampler))
-            l1_loss_img = l1_loss_img / (3 * len(data_handler.img_test_loader.sampler))
-            r_txt = r_txt / len(data_handler.txt_test_loader.sampler)
-            r_img = r_img / len(data_handler.img_test_loader.sampler)
-
-            if (epoch + 1) % 100 == 0:
-                print(
-                    'Epoch: {} \tTrain Loss: {:.3f} \tVal Loss: {:.3f} \ttxt Loss: {:.3f}, \timg Loss: {:.3f} \ttxt r: {:.4f}, \timg r: {:.4f}'.format(
-                        epoch + 1,
-                        train_loss,
-                        valid_loss,
-                        l1_loss_txt,
-                        l1_loss_img,
-                        r_txt,
-                        r_img,
-                    )
+        if (epoch + 1) % 100 == 0:
+            print(
+                'Epoch: {} \tTrain Loss: {:.3f} \tVal Loss: {:.3f} \ttxt Loss: {:.3f}, \timg Loss: {:.3f} \ttxt r: {:.4f}, \timg r: {:.4f}'.format(
+                    epoch + 1,
+                    train_loss,
+                    valid_loss,
+                    l1_loss_txt,
+                    l1_loss_img,
+                    r_txt,
+                    r_img,
                 )
+            )
 
-                # save model
-                if valid_loss <= valid_loss_min:
-                    torch.save(mlp.state_dict(), 'outputs/model_mixed.pt')
-                    valid_loss_min = valid_loss
-
-
-#         loss_list.append(l1_loss)
-
-#     loss_lists.append(loss_list)
-
-
-# import plotly.express as px
-
-# data = []
-# for n in range(len(hhh)):
-#     for m in range(config.num_trials):
-#         data.append({'hh': str(hhh[n]) ,'loss': loss_lists[n][m]})
-# import pandas as pd
-# df = pd.DataFrame(data)
-
-# print(df)
-
-# fig = px.box(df,x='hh', y='loss')
-# fig.show()
+            # save model
+            if valid_loss <= valid_loss_min:
+                torch.save(mlp.state_dict(), 'data/model_mixed.pt')
+                valid_loss_min = valid_loss
