8f7b9f01c071b314b5f7d95bfbef6d5286ad4b05
francisco <franciscoj.iba@gmail.com>
2023-01-13 11:17:15 +1100

    updated diffusion generators

--- diffuse_many.py
+++ diffuse_many.py
@@ -1,6 +1,7 @@
 import torch
 import clip
 import pickle
+import os
 from src.mlp import MLP
 from torchvision.transforms import Resize
 from stable_diffusion.scripts.stable_diffuser import StableDiffuser
@@ -8,8 +9,11 @@ from stable_diffusion.scripts.stable_diffuser import StableDiffuser
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-NUM_IMGS = 100
+NUM_IMGS = 6
 BATCH_SIZE = 6
+# METHOD = 'full_vec'
+METHOD = 'single_dim'
+W = 0.2
 
 resize = Resize(size=224)
 clip_model = clip.load('ViT-B/32', jit=False)[0].eval().requires_grad_(False).to(device)
@@ -19,14 +23,14 @@ with open('data/data_handler_mixed.pkl', 'rb') as f:
     data_handler = pickle.load(f)
 
 PROMPTS = [
-    # "A dog in the forest",
-    # "A meal on a white plate",
-    # "A tree on a hilltop",
+    "A dog in the forest",
+    "A meal on a white plate",
+    "A tree on a hilltop",
     "A forest",
     "A dark forest",
     "A house overlooking the sea",
     "An old house overlooking the sea",
-    "A colorful wild animal",
+    "A colourful wild animal",
     "A large wild animal",
     "A spaceship",
     "An UFO",
@@ -45,25 +49,31 @@ Vs = {
     'no_aff': [],
 }
 ## Initialization
-stable_diffuser = StableDiffuser(outdir="results/diff_large_set")
+stable_diffuser = StableDiffuser(outdir=f"results/diff_large_set2/{METHOD}_{int(10*W)}")
 with torch.no_grad():
     start_code = torch.randn([NUM_IMGS, 4, 512 // 8, 512 // 8], device='cpu')
     for v_name in Vs:
         for prompt in PROMPTS:
-            num_imgs = 0
-            while num_imgs < NUM_IMGS:
-                stable_diffuser.initialize(
-                    prompt=prompt,
-                    start_code=start_code[
-                        num_imgs : min(num_imgs + BATCH_SIZE, NUM_IMGS), :, :, :
-                    ],
-                )
+            os.makedirs(f"results/diff_large_set2/{METHOD}_{int(10*W)}/{prompt.replace(' ','_')}", exist_ok=True)
+            stable_diffuser.outpath = f"results/diff_large_set2/{METHOD}_{int(10*W)}/{prompt.replace(' ','_')}"
+            try:
                 if not v_name == 'no_aff':
                     with open(
-                        f"data/diff_embeddings/{prompt.replace(' ','_')}_{v_name}_010_03.pkl",
+                        f"data/diff_embeddings2/{METHOD}_{int(10*W)}/{prompt.replace(' ','_')}/{v_name}.pkl",
                         'rb',
                     ) as f:
                         z = pickle.load(f)
-                    stable_diffuser.override_zz(z)
-                stable_diffuser.run_diffusion(suffix=v_name)
-                num_imgs += BATCH_SIZE
+                num_imgs = 0
+                while num_imgs < NUM_IMGS:
+                    stable_diffuser.initialize(
+                        prompt=prompt,
+                        start_code=start_code[
+                            num_imgs : min(num_imgs + BATCH_SIZE, NUM_IMGS), :, :, :
+                        ],
+                    )
+                    if not v_name == 'no_aff':
+                        stable_diffuser.override_zz(z)
+                    stable_diffuser.run_diffusion(suffix=v_name)
+                    num_imgs += BATCH_SIZE
+            except:
+                continue
--- gen_diffusion.py
+++ gen_diffusion.py
@@ -1,11 +1,14 @@
 import pickle
 import torch
 import copy
+import os
 from src.mlp import MLP
 
 
-W = 0.3
-MAX_ITER = 5000
+W = 0.2
+# METHOD = 'full_vec'
+METHOD = 'single_dim'
+MAX_ITER = 500
 LR = 0.1
 PROMPTS = [
     'A dog in the forest',
@@ -39,18 +42,18 @@ mlp = MLP([64, 32], do=True, sig=False, h0=768).to(device)
 criterion = torch.nn.MSELoss(reduction='mean')
 mean_error = torch.nn.L1Loss(reduction='mean')
 
+import os
+
+
 for prompt in PROMPTS:
     print(f"----- {prompt} -----")
     z_0 = data_handler.model.get_learned_conditioning([prompt])
     z_0.requires_grad = False
     for v_name in Vs:
         print(f"----- {v_name} -----")
-        # v = torch.tensor([Vs[v_name][1]], device=device)
-        # v = torch.tensor([Vs[v_name]], device=device)
-        # v_idx = Vs[v_name][0]
         zz = torch.zeros_like(z_0)
         for channel in range(77):
-            print(f"----- Adjusting Channel {channel} -----")
+            print(f"Adjusting Channel {channel} ...")
 
             with open(f'data/bert_nets/data_handler_bert_{channel}.pkl', 'rb') as f:
                 data_handler = pickle.load(f)
@@ -65,24 +68,29 @@ for prompt in PROMPTS:
             v = [v_0[k] if Vs[v_name][k] is None else Vs[v_name][k] for k in range(3)]
             v = torch.tensor([v], device=device)
 
-            for iter in range(MAX_ITER):
-                opt.zero_grad()
-
-                loss = 0
-                loss += criterion(z, z_0[:, channel, :])
-                loss += W * criterion(mlp(data_handler.scaler_Z.scale(z)), v)
-                loss.backward()
-                opt.step()
+            if v_name != 'no_aff':
+                for iter in range(MAX_ITER):
+                    opt.zero_grad()
+                    loss = 0
+                    loss += criterion(z, z_0[:, channel, :])
+                    if METHOD == 'full_vec':
+                        loss += W * criterion(mlp(data_handler.scaler_Z.scale(z)), v)
+                    elif METHOD == 'single_dim':
+                        dim = 0 if v_name[-1] == 'E' else (1 if v_name[-1] == 'P' else 2)
+                        loss += W * criterion(mlp(data_handler.scaler_Z.scale(z))[:, dim], v[:, dim])
+                    loss.backward()
+                    opt.step()
 
             with torch.no_grad():
                 zz[0, channel, :] = copy.deepcopy(z.detach())
+            
+            # print('dim: ', dim)
+            # print('mlp(z0): ', mlp(data_handler.scaler_Z.scale(z_0[:, channel, :])))
+            # print('mlp(z)', mlp(data_handler.scaler_Z.scale(z)))
 
         zz = zz.to('cpu')
+        os.makedirs(f"data/diff_embeddings2/{METHOD}_{int(10*W)}/{prompt.replace(' ','_')}/", exist_ok=True)
         with open(
-            f"data/diff_embeddings/{prompt.replace(' ','_')}_{v_name}_010_03.pkl", 'wb'
+            f"data/diff_embeddings2/{METHOD}_{int(10*W)}/{prompt.replace(' ','_')}/{v_name}.pkl", 'wb'
         ) as f:
             pickle.dump(zz, f)
-
-    z_0 = z_0.to('cpu')
-    with open(f"data/{prompt.replace(' ', '_')}_z0_010_03.pkl", 'wb') as f:
-        pickle.dump(z_0, f)
