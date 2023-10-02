fixed scaling

experiments/bunch_diffusion_survey.py
@@ -55,7 +55,7 @@ for prompt in PROMPTS:
     )
 
     for aff_idx in range(3):
-        for aff_val in [0.00, 0.5, 1]:
+        for aff_val in [0.00, 0.5, 0.95]:
             v_name = f"{aff_names[aff_idx]}_{round(100*aff_val)}"
             aff_val = torch.tensor(aff_val, device=device, requires_grad=False)
 

src/data_handler_bert.py
@@ -40,7 +40,7 @@ class DataHandlerBERT:
         )
 
     @torch.no_grad()
-    def preprocess(self, savepath, word_type=None, scaling='none', word_batch_size=2048, channel=9):
+    def preprocess(self, savepath, word_type=None, z_scaling='none', v_scaling='none', word_batch_size=2048, channel=9):
         torch.cuda.empty_cache()
         fil_df = self.df[self.df["Type"] == word_type] if word_type else self.df
         self.words = list(fil_df["Word"])
@@ -71,7 +71,8 @@ class DataHandlerBERT:
             "Z": Z,
             "V": V,
             "Vsd": Vsd,
-            "scaling": scaling
+            "z_scaling": z_scaling,
+            "v_scaling": v_scaling,
         }
         with open(savepath, 'wb') as f:
             pickle.dump(data, f)    
@@ -83,8 +84,8 @@ class DataHandlerBERT:
         self.Z = data["Z"]
         self.V = data["V"]
         self.Vsd = data ["Vsd"]
-        self.scaler_Z = Scaler(self.Z, data["scaling"])
-        self.scaler_V = Scaler(self.V, data["scaling"])
+        self.scaler_Z = Scaler(self.Z, data["z_scaling"])
+        self.scaler_V = Scaler(self.V, data["v_scaling"])
 
     @torch.no_grad()
     def build_datasets(self, train_ratio=0.7, batch_size=512):

train_with_bert.py
@@ -24,7 +24,7 @@ parser.add_argument(
     "--use_sigmoid",
     type=bool,
     help="Use sigmoid at the end of last layer?",
-    default=False,
+    default=True,
 )
 config = parser.parse_args()
 layer_dims = [int(x) for x in config.layer_dims.split('|')]
@@ -39,7 +39,7 @@ loss_hist = [[] for x in range(77)]
 for channel in range(77):
     print(f"----- Training channel {channel} -----")
     path = f"data/bert_nets/data_ch_{channel}.pkl"
-    data_handler.preprocess(savepath=path,scaling="uniform")
+    data_handler.preprocess(savepath=path, z_scaling="none", v_scaling="uniform")
     data_handler.load_data(savepath=path)
     data_handler.build_datasets()
     
@@ -70,11 +70,11 @@ for channel in range(77):
             mlp.eval()  # prep model for evaluation
             for data, label, sds in data_handler.test_loader:
                 output = mlp(data)
-                label = data_handler.scaler_V.unscale(label)
-                output = data_handler.scaler_V.unscale(output)
+                # label = data_handler.scaler_V.unscale(label)
+                # output = data_handler.scaler_V.unscale(output)
                 loss = criterion(output, label)
                 valid_loss += loss.item() * data.size(0)
-                l1_loss_txt += torch.sum(torch.abs(output - label) / 8.6).item()
+                l1_loss_txt += torch.sum(torch.abs(output - label)).item()
 
             train_loss = train_loss / len(data_handler.train_loader.sampler)
             valid_loss = valid_loss / len(data_handler.test_loader.sampler)