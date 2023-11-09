44ef24e85ecee4905818bb6c25b46d25db362fab
fibarrola <francisco.ibarrola@outlook.com>
2023-11-09 11:51:57 +1100

    sortin analysis

--- experiments/analyze_survey.py
+++ experiments/analyze_survey.py
@@ -2,26 +2,30 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-df = pd.read_csv("data/v2_responses.csv")
-new_names = {col_name:f"{col_name[:2]}_{'most' if k%2==0 else 'least'}" for k, col_name in enumerate(df.columns) if k>=4 and k<=39}
+df = pd.read_csv("data/Affect_SQ (Responses) - Form responses 1.csv")
+df = df.dropna()
+# print(df.columns)
+print(len(df))
+new_names = {col_name:f"{col_name[:2]}_{'most' if k%2==1 else 'least'}" for k, col_name in enumerate(df.columns) if col_name[4:8]=="Pick"}
 df = df.rename(columns=new_names)
-new_names = {col_name:f"{col_name[:2]}" for k, col_name in enumerate(df.columns) if k>=40}
+df = df.rename(columns={df.columns[4]:"computer/phone"})
+new_names = {col_name:col_name[:2] for col_name in df.columns if col_name[4:7]=="How"}
 df = df.rename(columns=new_names)
-
-print(df.columns)
+# print(df.columns)
+print(df)
 
 #
 # Sorting
 #
 sorting_data = []
-for k in range(18):
+for k in range(24):
     for choice_of in ["most", "least"]:
         col_name = "{number:02d}_{choice_of:}".format(number=k+1, choice_of=choice_of)
-        generator = "VQGAN+CLIP" if (k//3) % 2 ==0 else "StableDifussion"
-        affect_dim = "Valence" if (k//6) % 3 ==0 else ("Arousal" if (k//6) % 3 ==1 else "Dominance")
+        generator = "VQGAN+CLIP" if (k//4) % 2 ==0 else "StableDifussion"
+        affect_dim = "Valence" if (k//8) % 3 ==0 else ("Arousal" if (k//8) % 3 ==1 else "Dominance")
         right_answer = df[col_name].iloc[0]
+        # print(generator, affect_dim, right_answer)
         for n in range(1, len(df)):
-
             if df[col_name].iloc[n] == right_answer:
                 correctness = "correct"
             else:
@@ -37,6 +41,7 @@ for k in range(18):
             })
 
 sorting_data = pd.DataFrame(sorting_data)
+print(sorting_data)
 # fig = px.histogram(sorting_data, x="correctness", color="generator", histnorm="percent")
 fig = go.Figure()
 for generator in ["VQGAN+CLIP", "StableDifussion"]:
@@ -44,7 +49,7 @@ for generator in ["VQGAN+CLIP", "StableDifussion"]:
     xx = ["correct", "close", "terrible"]
     yy = [len(aux[aux["correctness"]==correctness])/len(aux) for correctness in xx]
     fig.add_trace(go.Bar(x = xx, y = yy, name=generator))
-# fig.show()
+fig.show()
 
 fig = go.Figure()
 for aff_dim in ["Valence", "Arousal", "Dominance"]:
@@ -52,7 +57,9 @@ for aff_dim in ["Valence", "Arousal", "Dominance"]:
     xx = ["correct", "close", "terrible"]
     yy = [len(aux[aux["correctness"]==correctness])/len(aux) for correctness in xx]
     fig.add_trace(go.Bar(x = xx, y = yy, name=aff_dim))
-# fig.show()
+fig.show()
+
+assert False
 
 #
 # Rating
